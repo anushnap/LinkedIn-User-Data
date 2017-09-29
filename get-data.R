b22e502c6053622757752d4fc1fb115b27307843
Anushna Prakash <anushnap@uw.edu>
2017-09-29 12:26:57 -0700

    Initialize git-data file

---
+++ get-data.R
@@ -0,0 +1,9 @@
+# Anushna Prakash
+# Research for Anna Fung
+# Get data on users fron LinkedIn about gender, location, etc.
+# Use Face++ API to estimate gender from LinkedIn profile pictures
+
+setwd("~/Research/LinkedIn-User-Data")
+
+library(RJSONIO)
+library(RCurl)
